45babaaf817fa7b3cf72ce5b9b9f1f730cf567da
wykhuh <wykhuh@users.noreply.github.com>
2014-11-20 11:30:45 -0800

    (cleanup) clean up default scores files. Add hardcoded scores.

--- public/scores/scores-controller.js
+++ public/scores/scores-controller.js
@@ -4,17 +4,11 @@ angular.module('myapp')
   .controller('ScoresController', ['$scope', '$modal', 'resolvedScores', 'Scores',
     function ($scope, $modal, resolvedScores, Scores) {
 
-      $scope.scores = resolvedScores;
+      // $scope.scores = resolvedScores;
 
-      $scope.create = function () {
-        $scope.clear();
-        $scope.open();
-      };
-
-      $scope.update = function (id) {
-        $scope.scores = Scores.get({id: id});
-        $scope.open(id);
-      };
+      $scope.scores = [{score: 10, questionsCount: 3, date: Date()}, 
+                       {score: 30, questionsCount: 4, date: Date()},
+                      {score: 20, questionsCount: 5, date: Date()}]
 
       $scope.delete = function (id) {
         Scores.delete({id: id},
@@ -23,68 +17,4 @@ angular.module('myapp')
           });
       };
 
-      $scope.save = function (id) {
-        if (id) {
-          Scores.update({id: id}, $scope.scores,
-            function () {
-              $scope.scores = Scores.query();
-              $scope.clear();
-            });
-        } else {
-          Scores.save($scope.scores,
-            function () {
-              $scope.scores = Scores.query();
-              $scope.clear();
-            });
-        }
-      };
-
-      $scope.clear = function () {
-        $scope.scores = {
-          
-          "score": "",
-          
-          "date": "",
-          
-          "questionsCount": "",
-          
-          "id": ""
-        };
-      };
-
-      $scope.open = function (id) {
-        var scoresSave = $modal.open({
-          templateUrl: 'scores-save.html',
-          controller: 'ScoresSaveController',
-          resolve: {
-            scores: function () {
-              return $scope.scores;
-            }
-          }
-        });
-
-        scoresSave.result.then(function (entity) {
-          $scope.scores = entity;
-          $scope.save(id);
-        });
-      };
-    }])
-  .controller('ScoresSaveController', ['$scope', '$modalInstance', 'scores',
-    function ($scope, $modalInstance, scores) {
-      $scope.scores = scores;
-
-      
-      $scope.dateDateOptions = {
-        dateFormat: 'yy-mm-dd',
-        
-        
-      };
-
-      $scope.ok = function () {
-        $modalInstance.close($scope.scores);
-      };
-
-      $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
-      };
     }]);
