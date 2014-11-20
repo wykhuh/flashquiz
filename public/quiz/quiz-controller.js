efcb67e2ebae137d2509f71740941f87737aaa72
wykhuh <wykhuh@users.noreply.github.com>
2014-11-20 12:23:50 -0800

    (feat/quiz) Quiz shows question from the database.

--- public/quiz/quiz-controller.js
+++ public/quiz/quiz-controller.js
@@ -1,14 +1,13 @@
 'use strict';
 
 angular.module('myapp')
-  .controller('QuizController', ['$scope', '$location',
-    function ($scope, $location) {
+  .controller('QuizController', ['$scope', '$location', 'Questions','Quiz',
+    function ($scope, $location, Questions, Quiz) {
+      var allQuestions = [];
 
-      // $scope.scores = resolvedScores;
-
-      $scope.question = {question: 'How many states', 
-                          options: ['50', '11','12','23']
-                        }
-
+      Questions.query().$promise.then(function(questions){
+        allQuestions = Quiz.processQandA(questions);
+        $scope.question = allQuestions[0];
+      });
 
     }]);
--- public/quiz/quiz-service.js
+++ public/quiz/quiz-service.js
@@ -3,4 +3,25 @@
 angular.module('myapp')
   .factory('Quiz', ['$resource', function ($resource) {
 
+
+    // reorganizes the questions data from the database 
+    var processQandA = function (questions) {
+        var list = [];
+        var options = [];
+  
+
+  
+        // shuffle the order of the options so correctAnswer isn't always first
+        questions.forEach(function (question) {
+            options = [question.correctAnswer, question.option1, question.option2, question.option3];
+            list.push({question: question.question, options: options, correct: question.correctAnswer});
+        });
+  
+        return list;
+    };
+
+    return {
+        processQandA: processQandA
+    };
+
   }]);
